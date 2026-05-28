45e34f7b70706bb3344254cf8ca4202c26b393cf
Dhruv Khare <dhruvkhare536@gmail.com>
2026-05-28 17:26:01 +0530

    updated backend url

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,9 +1,5 @@
 export const corsOptions={
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
-      process.env.CLIENT_URL,
-    ],
+    origin:"*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
