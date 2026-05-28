0ffaabe8ef47ab168e567a86d35ef264b3d76fcf
Dhruv Khare <dhruvkhare536@gmail.com>
2026-05-28 20:22:38 +0530

    updated cors options

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,5 +1,5 @@
 export const corsOptions={
-    origin:"https://patrachar.netlify.app",
+    origin:"*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
