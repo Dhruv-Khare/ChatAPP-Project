3c1a37b9972cd71bcb50cc8b165815dec4f4785d
Dhruv Khare <dhruvkhare536@gmail.com>
2026-05-28 18:34:25 +0530

    update backend url

--- client/src/contants/config.js
+++ client/src/contants/config.js
@@ -1,10 +1,1 @@
-export const server=import.meta.env.VITE_SERVER;
-
-
-//fix alert listener done
-//add refetch listener done
-//something went wrong on accepting frnd req done
-//data is not loading on login time  done
-//disbale login and register button 
-//tehre is erro that the page is not updating after adding member and removing member in Group
-//error in the page as when we add member teh messag eis not coming in the group
+export const server = "https://15-207-16-76.nip.io";
